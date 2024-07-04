b5dd9eff25ca0caede452a5cea330dd15064530b
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-04 04:10:52 -0700

    intall flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,17 @@
 # install a package flash from pip3
 
-package { 'python3-pip':
-  ensure   => 'present',
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
 }
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask']
 }
