0af6c7cdfe75ccbefc6c27114657f192b4f47094
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-19 01:20:36 -0700

    client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,41 @@
 #!/usr/bin/env bash
 # Ensure the /etc/ssh/ssh_config file exists
-file { '/etc/ssh/ssh_config':
-	ensure	=> 'present',
-}
+
+include stdlib
 
 # Add the configuration to disable password authentication
-file_line { 'Turn off passwd auth':
-	path	=> '/etc/ssh/ssh_config',
-	line	=> 'PasswordAuthentication no',
-	match	=> 'PasswordAuthentication yes',
-	replace	=> true,
+file_line { 'SSH Private Key':
+  path	           => '/etc/ssh/ssh_config',
+  line	           => '    IdentityFile ~/.ssh/school',
+  match	           => '^[#]+[\s]*(?i)IndentityFile[\s]+~/.ssh/id_rsa$',
+  replace	           => true,
+  append_on_no_match => true
 }
 
-# Add the identity file configuration
-file_line { 'Declare identity file':
-	path	=> '/etc/ssh/ssh_config',
-	line	=> 'IdentityFile ~/.ssh/config',
-	match	=> '^IdentityFile',
-}	
+# Regex match explanation
+#
+# ^	beginning of the line
+# [#]*  atleat one hash character
+# [\s]* zero or more white space characters
+# (?i)IndetityFile case insensitive "IdentityFile"
+# [\s]+ at least one whitespace character
+# ~/.ssh/id_rsa The ssh private key file path we want to replace
+# $	end of the line
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+# Regex match explanation
+#
+# ^	beginning of the line
+# [#]*  atleat one hash character
+# [\s]* zero or more white space characters
+# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
+# [\s]+ at least one whitespace character
+# (yes|no) with the value "yes" or the value "no"
+# $	end of the line
